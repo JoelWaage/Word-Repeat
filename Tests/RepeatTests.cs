79900d372d3de84de26c5c763fb587267f848ae2
Joel Waage <joeltwaage@gmail.com>
2016-07-25 18:08:30 -0700

    second spec passes true and false testing

--- Tests/RepeatTests.cs
+++ Tests/RepeatTests.cs
@@ -11,11 +11,23 @@ namespace WordRepeat
       Repeat testRepeat = new Repeat("a", "a");
       Assert.Equal(true, testRepeat.CountRepeats());
     }
-
     [Fact]
-    public void CountRepeats_IsSingleLetterRepeat_false()
+    public void CountRepeats_IsSingleLetterNotRepeat_false()
     {
       Repeat testRepeat = new Repeat("a", "b");
+      Assert.Equal(false, testRepeat.CountRepeats());
+    }
+
+    [Fact]
+    public void CountRepeats_IsStringRepeat_true()
+    {
+      Repeat testRepeat = new Repeat("ab", "ab");
+      Assert.Equal(true, testRepeat.CountRepeats());
+    }
+    [Fact]
+    public void CountRepeats_IsStringNotRepeat_false()
+    {
+      Repeat testRepeat = new Repeat("ab", "cd");
       Assert.Equal(false, testRepeat.CountRepeats());
     }
 
